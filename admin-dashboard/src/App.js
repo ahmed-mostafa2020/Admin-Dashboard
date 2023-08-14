c92448e03a41c32f2bf979efc7a1c5a97f711ae5
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-14 06:43:47 +0300

    will start in components

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <div className="App">hi</div>;
-}
+import React from "react";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
 
 export default App;
