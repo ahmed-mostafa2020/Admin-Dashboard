53c271178cc0e6e6dcc93978273b657a614f1786
Ahmed Mostafa <ahmed-mostafa2020>
2023-08-17 15:20:40 +0300

    after adding bugsnag

--- admin-dashboard/src/index.js
+++ admin-dashboard/src/index.js
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+  apiKey: "56a2138d92e9b37bb5b57f03dc1c7bb9",
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
--- admin-dashboard/src/pages/home/Home.js
+++ admin-dashboard/src/pages/home/Home.js
@@ -13,6 +13,7 @@ import {
 import BarChartBox from "../../components/barChartBox/BarChartBox";
 import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import BigChartBox from "../../components/bigChartBox/BigChartBox";
+import Bugsnag from "@bugsnag/js";
 
 const Home = () => {
   return (
@@ -20,35 +21,27 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-
       <div className="box box2">
         <ChartBox {...chartBoxUser} />
       </div>
-
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-
       <div className="box box4">
         <PieChartBox />
       </div>
-
       <div className="box box5">
         <ChartBox {...chartBoxRevenue} />
       </div>
-
       <div className="box box6">
         <ChartBox {...chartBoxConversion} />
       </div>
-
       <div className="box box7">
         <BigChartBox />
       </div>
-
       <div className="box box8">
         <BarChartBox {...barChartBoxVisit} />
       </div>
-
       <div className="box box9">
         <BarChartBox {...barChartBoxRevenue} />
       </div>
